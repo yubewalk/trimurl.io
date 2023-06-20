29d046a945579e2918861047e2f63238572554f6
Ubon Asanga <ubon.asanga@gmail.com>
2023-06-20 17:02:45 +0100

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+//Connect to database
+connectDB();
 app.use(express.json({extend: false}));
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
