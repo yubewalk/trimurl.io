8dad4470f566728520818e263eb83ebaa5287f8a
Ubon Asanga <ubon.asanga@gmail.com>
2023-06-21 17:00:22 +0100

    Final Commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 const app = express();
+
 //Connect to database
 connectDB();
 app.use(express.json({extend: false}));
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
